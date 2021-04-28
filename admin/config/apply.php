06a03a1daa1c9c091ad8473675d171403b5e7ea8
thwass <79791690+thwass@users.noreply.github.com>
2021-04-28 20:17:08 +0530

    completed apply.php

--- admin/config/apply.php
+++ admin/config/apply.php
@@ -1,1 +1,14 @@
+<?php 
+
+     //Start Session Here
+    session_start();
+    include('functions.php');
+    //Set Default Time Zone
+    date_default_timezone_set('Asia/Kolkata');
+    $obj=new Functions();
+    //Connecting to Database
+    $conn=$obj->db_connect();
+    //SElecting Database
+    $db_select=$obj->db_select($conn);
+?>
 
