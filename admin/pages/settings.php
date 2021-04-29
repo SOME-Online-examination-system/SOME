15ca9708187851ab882ae57df46cc1e248a85e79
thwass <79791690+thwass@users.noreply.github.com>
2021-04-29 19:28:46 +0530

    updated and completed settings.php

--- admin/pages/settings.php
+++ admin/pages/settings.php
@@ -82,3 +82,114 @@
                     </form>
                     <hr />
                     -->
+                    <?php 
+                        if(isset($_POST['submit']))
+                        {
+                            //echo "Clicked";
+                           $app_name=$obj->sanitize($conn,$_POST['app_name']);
+                           $email=$obj->sanitize($conn,$_POST['email']);
+                           $username=$obj->sanitize($conn,$_POST['username']);
+                           $contact=$obj->sanitize($conn,$_POST['contact']);
+                           $current_password=$obj->sanitize($conn,$_POST['current_password']);
+                           
+                           //Normal Validation
+                           if(($app_name=="")or($email=="")or($username=="")or($contact=="")or($current_password==""))
+                           {
+                                $_SESSION['validation']="<div class='error'>App Name or Email or Username or Contact or Password is Empty.</div>";
+                                header('location:'.SITEURL.'admin/index.php?page=settings');
+                           }
+                           if($current_password==$password)
+                           {
+                                $tbl_name="tbl_app";
+                            $data="
+                                app_name='$app_name',
+                                email='$email',
+                                username='$username',
+                                contact='$contact'
+                            ";
+                            $where="app_id=$app_id";
+                            $query=$obj->update_data($tbl_name,$data,$where);
+                            $res=$obj->execute_query($conn,$query);
+                            if($res===true)
+                            {
+                                $_SESSION['update']="<div class='success'>App details successfully updated.</div>";
+                                header('location:'.SITEURL.'admin/index.php?page=settings');
+                            }
+                            else
+                            {
+                                $_SESSION['update']="<div class='error'>Failed to update app details.</div>";
+                                header('location:'.SITEURL.'admin/index.php?page=settings');
+                            }
+                           }
+                           else{
+                            $_SESSION['invalid']="<div class='error'>Current Password did not match.</div>";
+                                header('location:'.SITEURL.'admin/index.php?page=settings');
+                           }
+                        }
+                    ?>
+                </div>
+                
+                <div class="report">
+                    
+                    <form method="post" action="" class="forms">
+                        <h2>Change Password</h2>
+                        
+                        <span class="name">Current Password</span>
+                        <input type="password" name="current_password" placeholder="Current Password" required="true" /><br />
+                        
+                        <span class="name">New Password</span>
+                        <input type="password" name="new_password" placeholder="New Password" required="true" /><br />
+                        
+                        <span class="name">Confirm Password</span>
+                        <input type="password" name="confirm_password" placeholder="Confirm Password" required="true" /><br />
+                        
+                        
+                        <input type="submit" name="update" value="Update Password" class="btn-update" style="margin-left: 15%;" />
+                        <a href="<?php echo SITEURL; ?>admin/index.php"><button type="button" class="btn-delete">Cancel</button></a>
+                    </form>
+                    
+                    <?php 
+                        if(isset($_POST['update']))
+                        {
+                            //echo "Clicked";
+                            //Get Details from forms
+                            $new_password=md5($obj->sanitize($conn,$_POST['new_password']));
+                            $confirm_password=md5($obj->sanitize($conn,$_POST['confirm_password']));
+                            $current_password=md5($obj->sanitize($conn,$_POST['current_password']));
+                            if($current_password==$password)
+                            {
+                                if($new_password==$confirm_password)
+                                {
+                                    $tbl_name='tbl_app';
+                                    $data="password='$new_password'";
+                                    $where="app_id='$app_id'";
+                                    $query=$obj->update_data($tbl_name,$data,$where);
+                                    $res=$obj->execute_query($conn,$query);
+                                    if($res==true)
+                                    {
+                                        $_SESSION['password']="<div class='success'>Password changed successfully.</div>";
+                                        header('location:'.SITEURL.'admin/index.php?page=settings');
+                                    }
+                                    else
+                                    {
+                                        $_SESSION['password']="<div class='error'>Failed to change password. Try again.</div>";
+                                        header('location:'.SITEURL.'admin/index.php?page=settings');
+                                    }
+                                }
+                                else
+                                {
+                                    $_SESSION['not_match']="<div class='error'>New Password and Confirm Password did not match.</div>";
+                                    header('location:'.SITEURL.'admin/index.php?page=settings');
+                                }
+                            }
+                            else
+                            {
+                                $_SESSION['not_match']="<div class='error'>Current Password did not match.</div>";
+                                header('location:'.SITEURL.'admin/index.php?page=settings');
+                            }
+                        }
+                    ?>
+                </div>
+            </div>
+        </div>
+        <!--Body Ends Here-->
