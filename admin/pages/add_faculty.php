fc77269e4d74c4bee12aead1d56c96c6096c669c
Rohan <Rohan.bt19cse@pec.edu.in>
2021-04-29 11:40:13 +0530

    added backend php part and completed add_faculty

--- admin/pages/add_faculty.php
+++ admin/pages/add_faculty.php
@@ -5,8 +5,7 @@
                     
                     <form method="post" action="" class="forms">
                         <h2>Add Faculty</h2>
-                      
-                            <?php 
+                        <?php 
                             if(isset($_SESSION['validation']))
                             {
                                 echo $_SESSION['validation'];
@@ -17,8 +16,7 @@
                                 echo $_SESSION['add'];
                                 unset($_SESSION['add']);
                             }
-                            ?>
-                            
+                        ?>
                         <span class="name">Faculty Title</span> 
                         <input type="text" name="faculty_name" placeholder="Faculty Title" required="true" /> <br />
                         
@@ -45,7 +43,59 @@
                         </a>
                     </form>
                     
-               
+                    <?php 
+                        if(isset($_POST['submit']))
+                        {
+                            //echo "Clicked";
+                            //Get Values froom the form
+                            $faculty_name=$obj->sanitize($conn,$_POST['faculty_name']);
+                            $time_duration=$obj->sanitize($conn,$_POST['time_duration']);
+                            $qns_per_page=$obj->sanitize($conn,$_POST['qns_per_page']);
+                            $total_english=$obj->sanitize($conn,$_POST['total_english_qns']);
+                            $total_math=$obj->sanitize($conn,$_POST['total_math_qns']);
+                            if(isset($_POST['is_active']))
+                            {
+                                $is_active=$obj->sanitize($conn,$_POST['is_active']);
+                            }
+                            else
+                            {
+                                $is_active="yes";
+                            }
+                            $added_date=date('Y-m-d');
+                            
+                            //Normal PHP Validation
+                            if(($faculty_name=="")||($time_duration=="")||($qns_per_page==""))
+                            {
+                                $_SESSION['validation']="<div class='error'>Faculty name or Time Duration or Question Per Page is Empty.</div>";
+                                header('location:'.SITEURL.'admin/index.php?page=add_faculty');
+                            }
+                            //Inserting into the database
+                            $tbl_name='tbl_faculty';
+                            $data="faculty_name='$faculty_name',
+                                    time_duration='$time_duration',
+                                    qns_per_set='$qns_per_page',
+                                    total_english='$total_english',
+                                    total_math='$total_math',
+                                    is_active='$is_active',
+                                    added_date='$added_date',
+                                    updated_date=''";
+                            //Query to Insert Data
+                            $query=$obj->insert_data($tbl_name,$data);
+                            $res=$obj->execute_query($conn,$query);
+                            if($res===true)
+                            {
+                                //Success Message
+                                $_SESSION['add']="<div class='success'>New faculty successfully added.</div>";
+                                header('location:'.SITEURL.'admin/index.php?page=faculties');
+                            }
+                            else
+                            {
+                                //FAil Message
+                                $_SESSION['add']="<div class='error'>Failed to add new faculty. Try again.</div>";
+                                header('location:'.SITEURL.'admin/index.php?page=add_faculty');
+                            }
+                        }
+                    ?>
                 </div>
             </div>
         </div>
