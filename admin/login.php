86dabf58571312a48535c063a43018f8703f7604
Rohan <Rohan.bt19cse@pec.edu.in>
2021-04-29 13:39:51 +0530

    made the structure, php portion left

--- admin/login.php
+++ admin/login.php
@@ -1,1 +1,29 @@
 
+<?php 
+    include('config/apply.php');
+    include('../box/header.php');
+?>
+
+<!--Body Starts Here-->
+        <div class="main">
+            <div class="login">
+                <form method="post" action="">
+                    <h2>Admin | Log In</h2>
+                    <?php 
+                        
+                    ?>
+                    <input type="text" name="username" placeholder="Username" required="true" />
+                    <input type="password" name="password" placeholder="Password" required="true" />
+                    <input type="submit" name="submit" value="Log In" class="btn-go" />
+                    <input type="reset" name="reset" value="Reset" class="btn-exit" />
+                </form>
+                <?php 
+                    
+                ?>
+            </div>
+        </div>
+        <!--Body Ends Here-->
+
+<?php
+    include('../box/footer.php');
+?>
