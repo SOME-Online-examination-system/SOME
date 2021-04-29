c53906008063027d66cc5951d8f1041870c116c4
a007vipul <80327036+a007vipul@users.noreply.github.com>
2021-04-29 22:55:40 +0530

    completed view_result.php

--- admin/pages/view_result.php
+++ admin/pages/view_result.php
@@ -51,6 +51,115 @@
                             $fifth_answer=$row2['fifth_answer'];
                             $reason=$row2['reason'];
                             ?>
+                            
+                            <label style="font-weight: bold;"><?php echo $sn++.'. '.$question; ?></label><br />
+                            <?php 
+                                //To get usersAnswer
+                                
+                                //Using Switch Case
+                                switch($user_answer)
+                                {
+                                    case 0:
+                                    {
+                                        $userAnswer="None";
+                                    }
+                                    break;
+                                    
+                                    case 1:
+                                    {
+                                        $userAnswer=$first_answer;
+                                    }
+                                    break;
+                                    case 2:
+                                    {
+                                        $userAnswer=$second_answer;
+                                    }
+                                    break;
+                                    case 3:
+                                    {
+                                        $userAnswer=$third_answer;
+                                    }
+                                    break;
+                                    case 4:
+                                    {
+                                        $userAnswer=$fourth_answer;
+                                    }
+                                    break;
+                                    case 5:
+                                    {
+                                        $userAnswer=$fifth_answer;
+                                    }
+                                    break;
+                                }
+                                //To get rightAnswer
+                                switch($right_answer)
+                                {
+                                    case 0:
+                                    {
+                                        $rightAnswer="None";
+                                    }
+                                    break;
+                                    
+                                    case 1:
+                                    {
+                                        $rightAnswer=$first_answer;
+                                    }
+                                    break;
+                                    case 2:
+                                    {
+                                        $rightAnswer=$second_answer;
+                                    }
+                                    break;
+                                    case 3:
+                                    {
+                                        $rightAnswer=$third_answer;
+                                    }
+                                    break;
+                                    case 4:
+                                    {
+                                        $rightAnswer=$fourth_answer;
+                                    }
+                                    break;
+                                    case 5:
+                                    {
+                                        $rightAnswer=$fifth_answer;
+                                    }
+                                    break;
+                                }
+                            ?>
+                            <label>Users Answer: </label>
+                            <?php 
+                                if($userAnswer==$rightAnswer)
+                                {
+                                    ?>
+                                    <label style="color: green;"><?php echo $userAnswer; ?></label>
+                                    <?php
+                                }
+                                else
+                                {
+                                    ?>
+                                    <label style="color: red;"><?php echo $userAnswer; ?></label>
+                                    <?php
+                                }
+                            ?>
+                            
+                            <br />
+                            <label>Correct Answer:</label><label style="color: green;"><?php echo $rightAnswer; ?></label><br />
+                            
+                            
+                                <?php 
+                                    if($reason!="")
+                                    {
+                                        echo "<div class='success'>".$reason."</div>";
+                                    }
+                                ?>
+                            <hr />
+                            <?php
+                        }
+                    ?>
+                        <a href="<?php echo SITEURL; ?>admin/index.php?page=dashboard">
+                            <button type="button" class="btn-add">Go Home</button>
+                        </a>
                 </div>
             </div>
         </div>
