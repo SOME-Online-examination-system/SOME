8f74671ccae017495ca307043276a90762de05e6
Abhishek kumar <76047910+Abhi-Qbit-23@users.noreply.github.com>
2021-04-30 12:12:30 +0530

    Update main.php

--- pages/main.php
+++ pages/main.php
@@ -7,3 +7,29 @@
     {
         $page='welcome';
     }
+    
+    switch($page)
+    {
+        case "welcome":
+        {
+            include('welcome.php');
+        }
+        break;
+        
+        case "question":
+        {
+            include('question.php');
+        }
+        break;
+        
+        case "login":
+        {
+            include('login.php');
+        }
+        break;
+        
+        case "endSession":
+        {
+            include('endSession.php');
+        }
+        break;
