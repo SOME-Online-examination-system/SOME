e0f5a4ff5975b0d54f015a157f06873201aa5c9e
Abhishek kumar <76047910+Abhi-Qbit-23@users.noreply.github.com>
2021-04-30 12:13:24 +0530

    Update main.php

--- pages/main.php
+++ pages/main.php
@@ -33,3 +33,66 @@
             include('endSession.php');
         }
         break;
+        case "detail_result":
+        {
+            include('detail_result.php');
+        }
+        break;
+        
+        case "logout":
+        {
+            /*
+            if(isset($_SESSION['totalScore']))
+            {
+                $totalScore=$_SESSION['totalScore'];
+            }
+            else
+            {
+                $totalScore=0;
+            }
+            //To get student id from student username
+            $tbl_name="tbl_student";
+            $username=$_SESSION['student'];
+            $student_id=$obj->get_userid($tbl_name,$username,$conn);
+            //to add sesult summary to the database
+            $added_date=date('Y-m-d');
+            $tbl_name2="tbl_result_summary";
+            $data="student_id='$student_id',
+                    marks='$totalScore',
+                    added_date='$added_date'
+                    ";
+            $query=$obj->insert_data($tbl_name2,$data);
+            $res=$obj->execute_query($conn,$query);
+            */
+            $tbl_name="tbl_student";
+            $username=$_SESSION['student'];
+            $student_id=$obj->get_userid($tbl_name,$username,$conn);
+            $res=true;
+            if($res==true)
+            {
+                //Setting Student Is_Active Mode to No
+                $tbl_name3="tbl_student";
+                $data3="is_active='no'";
+                $where3="student_id='$student_id'";
+                $query3=$obj->update_data($tbl_name3,$data3,$where3);
+                $res3=$obj->execute_query($conn,$query3);
+                if($res3===true)
+                {
+                    session_destroy();
+                    header('location:'.SITEURL.'index.php?page=login');
+                }
+                else
+                {
+                    echo "Error";
+                }
+                
+            }
+            else
+            {
+                echo "Error";
+            }
+            
+        }
+        break;
+        
+        default:
