27db68aac877f2abfa89c1b2890a6e238c6b46d5
thwass <79791690+thwass@users.noreply.github.com>
2021-04-30 12:10:51 +0530

    created index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php 
+    require "navbar.php";
+    include('admin/config/apply.php');
+    ob_start();
+    include('box/header.php');
+    include('pages/main.php');
+    include('box/footer.php');
+?>
