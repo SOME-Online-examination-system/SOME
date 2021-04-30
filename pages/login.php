9e42ec6675b02d9124ac69c3aea1a6aa5272fa5f
Rohan <Rohan.bt19cse@pec.edu.in>
2021-04-30 09:55:47 +0530

    started working on login page

--- pages/login.php
+++ pages/login.php
@@ -1,1 +1,19 @@
-
+<!--Body Starts Here-->
+        <div class="main">
+            <div class="login">
+                <form method="post" action="">
+                    <h2>Log In</h2>
+                    <?php 
+                        if(isset($_SESSION['invalid']))
+                        {
+                            echo $_SESSION['invalid'];
+                            unset($_SESSION['invalid']);
+                        }
+                    ?>
+                    <input type="text" name="username" placeholder="Username" required="true" />
+                    <input type="password" name="password" placeholder="Password" required="true" />
+                    <input type="submit" name="submit" value="Log In" class="btn-go" />
+                    <input type="reset" name="reset" value="Reset" class="btn-exit" />
+                </form> 
+        </div>
+        <!--Body Ends Here-->
