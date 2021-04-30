b6b19b2e559ffab070f87722e680a024414d64fc
a007vipul <80327036+a007vipul@users.noreply.github.com>
2021-04-30 11:20:36 +0530

    completed footer.php
    added github links

--- box/footer.php
+++ box/footer.php
@@ -1,1 +1,11 @@
-
+<!--Footer Starts Here-->
+        <footer class="footer">
+            <div class="wrapper">
+                <p>
+                    <a href="https://github.com/SOME-Online-examination-system/" title="SOME">SOME</a> &copy; <?php echo date('Y'); ?>    
+                    by <a href="https://github.com/orgs/SOME-Online-examination-system/people" title="PEC" target="_blank">PEC</a>.
+                </p>
+            </div>
+        </footer>
+        <!--Footer Ends Here-->
+</html>
