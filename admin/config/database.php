c511f197dd37fb74ce991ba721a3e3267a037598
Abhishek kumar <76047910+Abhi-Qbit-23@users.noreply.github.com>
2021-04-30 15:45:35 +0530

    completed the php file database.php

--- admin/config/database.php
+++ admin/config/database.php
@@ -45,3 +45,105 @@
             $query="INSERT INTO $tbl_name SET $data";
             return $query;
         }
+        public function update_data($tbl_name,$data,$where="")
+        {
+            $query="UPDATE $tbl_name SET $data WHERE $where";
+            return $query;
+        }
+        public function delete_data($tbl_name,$where)
+        {
+            $query="DELETE FROM $tbl_name WHERE $where";
+            return $query;
+        }
+        public function execute_query($conn,$query)
+        {
+            $res=mysqli_query($conn,$query) or die(mysqli_error($conn));
+            return $res;
+        }
+        public function num_rows($res)
+        {
+            $num_rows=mysqli_num_rows($res);
+            return $num_rows;
+        }
+        public function fetch_data($res)
+        {
+            $row=mysqli_fetch_assoc($res);
+            return $row;
+        }
+        public function check_image_type($ext)
+        {
+            $valid=array('jpg','png','gif','JPG','PNG','GIF','JPEG');
+            if(in_array($ext,$valid))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public function check_file_type($ext)
+        {
+            $valid=array('pdf','docx','ppt','txt');
+            if(in_array($ext,$valid))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public function upload_file($source,$destination)
+        {
+            $upload=move_uploaded_file($source,$destination);
+            return $upload;
+        }
+        public function remove_file($path)
+        {
+            $remove=unlink($path);
+            return $remove;
+        }
+        public function get_total_rows($tbl_name,$conn)
+        {
+            $query="SELECT * FROM $tbl_name";
+            $res=mysqli_query($conn,$query) or die(mysqli_error($conn));
+            $rows=mysqli_num_rows($res);
+            return $rows;
+        }
+        public function get_userid($tbl_name,$username,$conn)
+        {
+            $query="SELECT student_id FROM $tbl_name WHERE username='$username'";
+            $res=mysqli_query($conn,$query) or die(mysqli_error($conn));
+            $row=mysqli_fetch_assoc($res);
+            $student_id=$row['student_id'];
+            return $student_id;
+        }
+        public function get_faculty($tbl_name,$student_id,$conn)
+        {
+            $query="SELECT faculty FROM $tbl_name WHERE student_id='$student_id'";
+            $res=mysqli_query($conn,$query) or die(mysqli_error($conn));
+            $row=mysqli_fetch_assoc($res);
+            $faculty=$row['faculty'];
+            return $faculty;
+        }
+        public function get_fullname($tbl_name,$student_id,$conn)
+        {
+            $query="SELECT first_name,last_name FROM $tbl_name WHERE student_id='$student_id'";
+            $res=mysqli_query($conn,$query) or die(mysqli_error($conn));
+            $row=mysqli_fetch_assoc($res);
+            $first_name=$row['first_name'];
+            $last_name=$row['last_name'];
+            $full_name=$first_name.' '.$last_name;
+            return $full_name;
+        }
+        public function get_facultyname($tbl_name,$faculty_id,$conn)
+        {
+            $query="SELECT faculty_name FROM $tbl_name WHERE faculty_id='$faculty_id'";
+            $res=mysqli_query($conn,$query) or die(mysqli_error($conn));
+            $row=mysqli_fetch_assoc($res);
+            $faculty_name=$row['faculty_name'];
+            return $faculty_name;
+        }
+    }
+?>
