72b274d41949e137444e687b855a5dbb3e9370b3
Rohan <Rohan.bt19cse@pec.edu.in>
2021-04-30 15:06:35 +0530

    Created sql file and table for student info

---
+++ quizapp/quizapp.sql
@@ -0,0 +1,40 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- PHP Version: 7.4.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `quizapp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_app`
+--
+
+CREATE TABLE `tbl_app` (
+  `app_id` int(10) UNSIGNED NOT NULL,
+  `app_name` varchar(255) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `contact` varchar(20) NOT NULL,
+  `added_date` date NOT NULL,
+  `updated_date` date NOT NULL,
+  `image_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
